39e5533da0b1f6b66d8f1a273c0e5a1a15023bf0
luchikAR <arbelenov@yandex.ru>
2022-06-18 16:04:58 +0300

    start

---
+++ srcs/src/main.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.*;
+
+public class main {
+    public static void main(String[] args){
+        // парсер
+        ArrayList<Integer> elements_degree2 = new ArrayList<Integer>();
+        ArrayList<Integer> elements_degree1 = new ArrayList<Integer>();
+        ArrayList<Integer> elements_degree0 = new ArrayList<Integer>();
+
+        // изначально натти номер строки, где =, так определять знак с кторым будет сохранять элементы
+        // .fide(X^2) и считать кол-во элементов, сколько всего X и сколько тех которые в степени 012, если иксов больше выдать ошибку.
+        // обычные числа считать отдельно
+        // если между числом и X^ пробел - выдать ошибку!
+        // решение уравнения
+    }
+}
